c2e2047889a83a685748095a4cc77f154e9a1f31
nabbehni24-del <nabbeh.ni24@gmail.com>
2026-09-17 21:55:13 +0200

    Use official Dorni logo in branding

--- components/dorni-brand.tsx
+++ components/dorni-brand.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
+import Image from "next/image";
 
 export function DorniBrand({ compact = false }: { compact?: boolean }) {
   return (
     <a className={`brand ${compact ? "brand-compact" : ""}`} href="/" aria-label="دورني">
-      <span className="brand-mark"><span /></span>
+      <Image className="brand-mark" src="/dorni-logo.svg" alt="" width={42} height={42} priority />
       <span><b>دورني</b><small>DORNI</small></span>
     </a>
   );
